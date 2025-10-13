967002db70ab31f4f3f41ae4b0713a71b74e06a2
helmholtz <helmholtz@fomal.host>
2025-10-13 14:26:25 +0800

    Initial Commit

---
+++ Paperkey/PaperkeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaperkeyApp.swift
+//  Paperkey
+//
+//  Created by 李昌埔 on 2025/10/13.
+//
+
+import SwiftUI
+
+@main
+struct PaperkeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
